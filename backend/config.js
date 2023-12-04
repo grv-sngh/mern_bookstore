3ced7c751e3e88e4b2d596cf591bf30fb3f97998
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-04 10:49:59 +0000

    Working on routes for CRUD
    Faced some difficulty with mongodb connection yet again but turns out it was related to the port issu, post method doesn't seem to work on the postman api maybe due to the codespace connection issue

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongodbURL = "";
+export const mongodbURL = "mongodb+srv://root:root@cluster0.lsejrc8.mongodb.net/nextjsblog?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,53 @@
-import express from "express";
-import { PORT } from "./config.js"; 
+import express, { request } from "express";
+import { PORT, mongodbURL } from "./config.js"; 
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// Middleware for parsing request body
+app.use(express.json());
+
+// Index get request
 app.get("/", (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to MERN")
 })
 
-
-app.listen(PORT, () => {
-    console.log(`App is listening at port ${PORT}`)
+// Get all books
+app.get("/books", async (request, response) => {
+    const books = await Book.find({});
+    return response.status(200).send(books);
 })
+
+// Get book by id
+app.get("/books/:id", async (request, response) => {
+    const { id } = params.id;
+    const books = await Book.findById(id);
+    return response.status(200).send(books);
+})
+
+// Create new book
+app.post("/books", async (request, response) => {
+    const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear
+    }
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+})
+
+
+
+mongoose
+    .connect(mongodbURL)
+    .then(() => {
+        console.log("Database connected");
+        app.listen(PORT, () => {
+            console.log(`App is listening at port ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
