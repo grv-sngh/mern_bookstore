c499d57c7a8d60266064ba5261b09326e0c189ae
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-07 15:47:47 +0000

    Added modal
    Learnt about the child and parents props and also displaying full size elements and the event propagation function

--- frontend/src/components/home/BooksSingleCard.jsx
+++ frontend/src/components/home/BooksSingleCard.jsx
@@ -5,8 +5,11 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 import { useState } from 'react';
+import BooksModal from './BooksModal';
+
 
 const BooksSingleCard = ({ book }) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
       <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>
@@ -22,6 +25,10 @@ const BooksSingleCard = ({ book }) => {
         <h2 className='my-1'>{book.author}</h2>
       </div>
       <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+        <BiShow
+          className='text-3xl text-blue-800 hover:text-black cursor-pointer'
+          onClick={() => setShowModal(true)}
+        />
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
         </Link>
@@ -32,6 +39,7 @@ const BooksSingleCard = ({ book }) => {
           <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
         </Link>
       </div>
+      {showModal && <BooksModal book={book} onClose={() => setShowModal(false)} />}
     </div>
   )
 }
