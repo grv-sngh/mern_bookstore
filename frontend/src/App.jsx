54770208f4c5b23aa1ad0ba92e2254b924d93439
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-08 10:48:18 +0000

    Added frontend routes for items
    Created routes for the frontend and the page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import CreateBook from "./pages/CreateBook";
 import EditBook from "./pages/EditBook";
 import ShowBook from "./pages/ShowBook";
 import DeleteBook from "./pages/DeleteBook";
+import HomeItem from "./pages/items/HomeItem";
+import CreateItem from "./pages/items/CreateItem"
+import ShowItem from "./pages/items/ShowItem"
+import EditItem from "./pages/items/EditItem"
+import DeleteItem from "./pages/items/DeleteItem"
 
 const App = () => {
   return(
@@ -14,6 +19,12 @@ const App = () => {
       <Route path="/books/details/:id" element={<ShowBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
       <Route path="/books/delete/:id" element={<DeleteBook />} />
+
+      <Route path="/items" element={<HomeItem />} />
+      <Route path="/items/create" element={<CreateItem />} />
+      <Route path="/items/details/:id" element={<ShowItem />} />
+      <Route path="/items/edit/:id" element={<EditItem />} />
+      <Route path="/items/delete/:id" element={<DeleteItem />} />
     </Routes>
   )
 }
