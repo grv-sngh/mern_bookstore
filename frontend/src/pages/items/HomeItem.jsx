371852a9e6361219237515491ba8d192fe435e7b
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-08 11:20:38 +0000

    Basic home for items
    Just testing out if the home page fetches the data and works as expected

--- frontend/src/pages/items/HomeItem.jsx
+++ frontend/src/pages/items/HomeItem.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const HomeItem = () => {
-  return (
-    <div>HomeItem</div>
-  )
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        axios
+            .get(`https://super-space-acorn-v9wxvwvw56g2p4gj-5555.app.github.dev/items`)
+            .then((response) => {
+                setItems(response.data.data)
+                console.log(response.data.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+    return (
+        <div>
+            Home Item
+            <ul>
+                {items.map((item) => (
+                    <li key={item._id}>
+                        <br />
+                        <p>{item._id}</p>
+                        <ul>
+                            <li>- {item.name}</li>
+                            <li>- {item.quantity}</li>
+                            <li>- {item.description}</li>
+                        </ul>
+                        <br />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default HomeItem
