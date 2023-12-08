cd1f1cc00605121bfb18b931513d189aecd906bf
Gaurav Singh <111694602+grv-sngh@users.noreply.github.com>
2023-12-08 09:57:44 +0000

    Added items model
    Created items schema and model

---
+++ backend/models/itemModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const itemSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+export const item = mongoose.model('Item', itemSchema);
